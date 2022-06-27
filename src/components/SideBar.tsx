1f76e6516ad2633bf7ddfa3eac9028d2754f9f4f
acferlucas <lafs@discente.ifpe.edu.br>
2022-06-27 14:24:27 -0300

    add codegen graphql-code-gen and classnames

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,33 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LEESON_QUERRY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      title
-      slug
-    }
-  }
-`;
-
-type Lesson = {
-  id: string;
-  title: string;
-  slug: string;
-  availableAt: string;
-  lessonType: 'live' | 'class';
-
-}
-
-type LessonsQuerryResponse = {
-  lessons: Lesson[]
-}
-
 export function SideBar(): JSX.Element {
-  const { data } = useQuery<LessonsQuerryResponse>(GET_LEESON_QUERRY);
+  const { data } = useGetLessonsQuery();
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-700">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,54 +1,27 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { CaretRight, FileArrowDown } from "phosphor-react";
 import { NavButton } from '../components/buttons/NavButton'
-import { gql, useQuery } from "@apollo/client";
 
 
 import '@vime/core/themes/default.css';
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
-const GET_LESSON_BY_SLUG_QUERRY = gql`
-  query GetLessonBySlug($slug: String) {
-  lesson(where: {slug: $slug}) {
-    title
-    videoId
-    description
-    teacher {
-      avatarURL
-      bio
-      name
-    }
-  }
-}
 
-`
 
 type VideoProps = {
   videoSlug: string;
 }
 
-type GetLessonBySlugResponse = {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      avatarURL: string;
-      bio: string;
-      name: string;
-    }
-  }
-}
-
 export function Video({ videoSlug }: VideoProps): JSX.Element {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERRY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
-      slug: videoSlug
+      slug: videoSlug,
     }
-  })
+  });
 
 
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1 flex items-center justify-center">
         <img
@@ -76,22 +49,26 @@ export function Video({ videoSlug }: VideoProps): JSX.Element {
             <p className="mt-4 text-gray-200 leading-relaxed">
               {data.lesson.description}
             </p>
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src={
-                  data.lesson.teacher.avatarURL
-                }
-                alt="perfil" />
+            {
+              data.lesson.teacher && (
+                <div className="flex items-center gap-4 mt-6">
+                  <img
+                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                    src={
+                      data.lesson.teacher.avatarURL
+                    }
+                    alt="perfil" />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{
-                  data.lesson.teacher.bio
-                }</span>
-              </div>
+                  <div className="leading-relaxed">
+                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                    <span className="text-gray-200 text-sm block">{
+                      data.lesson.teacher.bio
+                    }</span>
+                  </div>
 
-            </div>
+                </div>
+              )
+            }
           </div>
           <div className="flex flex-col gap-4">
             <NavButton type="discord" />
