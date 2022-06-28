c4800eaed002d3f281273602a71d3faa0c2939b0
acferlucas <lafs@discente.ifpe.edu.br>
2022-06-28 11:12:55 -0300

    create event responsivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
+import { ListPlus } from "phosphor-react";
 import { Icon } from "./Logo";
 
+type HeaderProps = {
+  handleChangeIsOpen: () => void;
+}
 
-export function Header(): JSX.Element {
+
+export function Header({ handleChangeIsOpen }: HeaderProps): JSX.Element {
   return (
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600" >
+    <header className="w-full px-5 md:px-0 py-5 flex items-center justify-between md:justify-center bg-gray-700 border-b border-gray-600" >
       <Icon />
+      <button className="md:hidden" onClick={handleChangeIsOpen} >
+        <ListPlus size={30} />
+      </button>
     </header>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,20 @@
 import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
+import classNames from "classnames";
 
-export function SideBar(): JSX.Element {
+type SideBarProps = {
+  isOpen: boolean;
+}
+
+
+export function SideBar({ isOpen }: SideBarProps): JSX.Element {
   const { data } = useGetLessonsQuery();
   return (
-    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-700">
+    <aside className={classNames("w-[348px] bg-gray-700 p-6 border-l border-gray-700 absolute md:block md:static", {
+      "right-0 z-[999]": isOpen,
+      "hidden": !isOpen
+
+    })}>
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
         Cronograma de aulas
       </span>
